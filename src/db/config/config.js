059d25fb117d9679d878390ed41bd1bc5eab5f86
brodrigues0ll <bernardorg99@hotmail.com>
2022-12-09 17:56:19 -0300

    fix: Começando a resolver conexão com DB

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,12 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const dev = {
-  username: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  dialect: "mysql"
+module.exports = {
+  development: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: "123",
+    database: "DB_TEST",
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mysql"
+  },
+  production: {
+    environment: "production"
+  }
 };
 
-module.exports = dev;
