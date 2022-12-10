0553c34ce8f01968793e704346263982d3c473bb
brodrigues0ll <bernardorg99@hotmail.com>
2022-12-09 22:15:33 -0300

    fix: Foi adicionado o cors ao código principal app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const cors = require('cors');
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(cors());
+
 
 const register = require('./routes/register');
 
-const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 
